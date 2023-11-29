96f35fa1724a202507948a8fb3d6bf0dac8bb255
Aymone SOH <aymonecynthiasoh@gmail.com>
2023-11-29 18:18:28 +0100

    deuxieme

--- OneDrive/Bureau/openclassrooms/crazee-burger/src/App.js
+++ OneDrive/Bureau/openclassrooms/crazee-burger/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Salut Vi
+          Salut Viale
         </p>
         <a
           className="App-link"
