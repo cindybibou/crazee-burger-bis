7f6d92e04803afa0e602e8045f45a0d4e187b614
Aymone SOH <aymonecynthiasoh@gmail.com>
2023-11-29 23:13:13 +0100

    suppression de App.test

--- OneDrive/Bureau/openclassrooms/crazee-burger/src/App.js
+++ OneDrive/Bureau/openclassrooms/crazee-burger/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Salut Viale
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //State(état,données): définitions de données dynamiques que l'on va projeter sur notre affichage
+const [prenom, setPrenom] = useState("Arthur")
+//Je définis un state que j'initialise avec la chaîne de caractère "Vi".
+//Que je récupère dans une variable prénom
+//Que je ne pourrais modifier que avec sa fonction dédiée setPrenom.
+const [count, setCount] = useState(1)
+
+
+
+//Comportements.On va définir de manière arbitraire ce qu'ils vont faire au niveau des instructions
+//Via un évènement
+const handleClick = () => {
+  setCount(count + 1) //Le setter reçoit en paramètre la new valeur du state
+} 
+
+
+
+  //Affichage (Render)Eléments d'affichage sur lesquels on va brancher des comportements
+return (
+  <div>
+    <h1>Bonjour {prenom} </h1>
+    <p>{count}</p>
+    <button onClick={handleClick}>Incrémenter</button>
+  </div>
+)
 }
 
-export default App;
+
+
+export default App
